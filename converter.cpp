93dfd53009a33d0ea8e5f218f30c0d38d524914c
Lars <l.p.1999@live.de>
2023-08-11 11:29:15 +0200

    fix

--- converter.cpp
+++ converter.cpp
@@ -94,17 +94,17 @@ QColor Converter::greyToColor(unsigned int grey)
         r = 255 * -cos(0.012368*(grey));
 
     g =   0.6 * 255 * sin(0.012368 * grey);
-//    if(r > 255)
-//        r = 255;
-//    if(g > 255)
-//        g = 255;
-//    if(b > 255)
-//        b = 255;
-//    if(r < 0)
-//        r = 0;
-//    if(g < 0)
-//        g = 0;
-//    if(b < 0)
-//        b = 0;
+    if(r > 255)
+        r = 255;
+    if(g > 255)
+        g = 255;
+    if(b > 255)
+        b = 255;
+    if(r < 0)
+        r = 0;
+    if(g < 0)
+        g = 0;
+    if(b < 0)
+        b = 0;
     return QColor(r, g, b);
 }
