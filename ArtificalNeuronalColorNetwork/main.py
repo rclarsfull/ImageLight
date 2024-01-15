edac32b4a8a1cd4bf8e2f181d961e985ecbbf2e7
Lars Przybylek <l.p.1999@live.de>
2024-01-15 14:02:58 +0100

    python server updated

--- ArtificalNeuronalColorNetwork/main.py
+++ ArtificalNeuronalColorNetwork/main.py
@@ -17,11 +17,15 @@ def handle_client(connection, client_address, model, x_scaler, X_RESELUTION, Y_R
         start_time = time.time()  # Record the start time
 
         # Receive the binary data from the socket
-        data_bytes = connection.recv(X_RESELUTION * Y_RESELUTION * 3 * 4)  # Assuming float is 4 bytes
-        if not data_bytes:
-            print("Error: No Data received")
-            return
-
+        data_bytes = b""
+        while len(data_bytes) < X_RESELUTION * Y_RESELUTION * 3 * 4:
+            chunk = connection.recv(X_RESELUTION * Y_RESELUTION * 3 * 4 - len(data_bytes))
+            if not chunk:
+                print(f"Error: No Data received from {client_address}")
+                return
+            data_bytes += chunk
+        print(f"Received {len(data_bytes)} bytes of data from {client_address}")
+        print(f"Expected size: {X_RESELUTION * Y_RESELUTION * 3 * 4} bytes")
         # Unpack the binary data into a float array using struct
         float_array = struct.unpack(f'{X_RESELUTION * Y_RESELUTION * 3}f', data_bytes)
 
@@ -33,25 +37,31 @@ def handle_client(connection, client_address, model, x_scaler, X_RESELUTION, Y_R
             rgbValues.append((numpy_array[counter], numpy_array[counter + 1], numpy_array[counter + 2]))
             counter += 3
         if len(rgbValues) != X_RESELUTION * Y_RESELUTION:
-            print("Error: Data amount doesn't match Resolution")
+            print(f"Error: Data amount doesn't match Resolution for {client_address}")
             return
 
+        
+        batch_rgb_values = np.array(rgbValues)
         feature_names = ['rot', 'gruen', 'blau']
-        rgbValues = pd.DataFrame(rgbValues, columns=feature_names)
+        rgbValues = pd.DataFrame(batch_rgb_values, columns=feature_names)
 
-        predictions = model.predict(x_scaler.transform(rgbValues))
+        predictions = model.predict(x_scaler.transform(batch_rgb_values))
+
         rounded_predictions = np.round(predictions)
 
         # Convert the rounded predictions to unsigned short values
-        scaled_predictions = rounded_predictions.astype(np.uint16)
-        scaled_predictions = scaled_predictions.flatten()
+        scaled_predictions = rounded_predictions.astype(np.uint16).flatten()
+
+        for i in range(scaled_predictions.size):
+            scaled_predictions[i] = (i % 1500) 
+        print(scaled_predictions)
 
         # Convert scaled_predictions to bytes
         scaled_bytes = struct.pack(f'{X_RESELUTION * Y_RESELUTION}H', *scaled_predictions)
 
         # Send the data over the connection
         connection.sendall(scaled_bytes)
-        print(f"Send {len(scaled_bytes)} bytes")
+        print(f"Send {len(scaled_bytes)} bytes to {client_address}")
 
     except Exception as e:
         print(f"Error handling connection from {client_address}: {str(e)}")
@@ -65,8 +75,8 @@ def handle_client(connection, client_address, model, x_scaler, X_RESELUTION, Y_R
         connection.close()
 
 def main():
-    server_ip = "127.0.0.1"
-    server_port = 6666
+    server_ip = "172.19.15.159"
+    server_port = 12345
     X_RESELUTION = 2352
     Y_RESELUTION = 1568
     print(f"Server expects an image size of {X_RESELUTION}x{Y_RESELUTION}")
